b52cc235c661cc7175ad821c8fa6d8a5b4e443a4
Minh Tram <minh.tram@mavs.uta.edu>
2021-10-26 00:20:45 -0500

    added Robotiq 2F messages

--- catkin_ws/src/unity_ros_backend/unity_planning_backend/scripts/urscript_test.py
+++ catkin_ws/src/unity_ros_backend/unity_planning_backend/scripts/urscript_test.py
@@ -11,13 +11,4 @@ if __name__ == '__main__':
     rospy.init_node('UR_ROS_dashboard_node', anonymous=True)
 
     dashboard = URDashboard()
-    
-    dashboard.load_installation(installation_name)
-    dashboard.cold_boot()
-    dashboard.load_program(program_name)
-    dashboard.start_program()
-    
-    input("Press Enter to continue...")
-
-    dashboard.stop_program()
-    dashboard.power_off_arm()
+    dashboard.release_brakes()
