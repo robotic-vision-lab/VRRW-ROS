8a2a30276127a8ae7e7eed065b7d0ba6a2feb08f
Minh Tram <minh.tram@mavs.uta.edu>
2021-12-09 19:34:52 -0600

    condensed modules

---
+++ catkin_ws/src/ur_remote_control/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['ur_remote_control'],
+    package_dir={'': 'src'}
+)
+
+setup(**d)
