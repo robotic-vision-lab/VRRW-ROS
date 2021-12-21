2c30e12576a05d9894a42881edb98fe8f012947c
Minh Tram <minh.tram@mavs.uta.edu>
2021-12-21 01:17:21 -0600

    added placeholder files

--- catkin_ws/src/ur_remote_control/nodes/Robotiq2FNode.py
+++ catkin_ws/src/ur_remote_control/nodes/Robotiq2FNode.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 
+# ROS packages
 import rospy
+from std_msgs.msg import Header
+from sensor_msgs.msg import JointState
+
+# Robotiq
+from robotiq_controller.RobotiqModbusServer import RobotiqRTUClient
+from ur_remote_control.msg import Robotiq2FCommand, Robotiq2FStatus
+
+def start_robotiq_backend(device_name, refresh_rate=200):
+    pass
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('robotiq_interface_node', anonymous=True)
+        gripper_addr = rospy.get_param('/ur_tool_communication_bridge/device_name', default='/tmp/ttyUR')
+        start_robotiq_backend(device_name=gripper_addr)
+    except Exception as e:
+        print(e)
+        pass
--- catkin_ws/src/ur_remote_control/src/robotiq_controller/RobotiqModbusServer.py
+++ catkin_ws/src/ur_remote_control/src/robotiq_controller/RobotiqModbusServer.py
@@ -13,7 +13,7 @@ class RobotiqRTUClient:
         self.unit_id = unit_id
         self.input_register_addr = input_addr
         self.output_register_addr = output_addr
-    
+
     def connect(self, device_addr, delay = 1):
         self.client = ModbusSerialClient(method = 'rtu',
                                          port = device_addr,
@@ -22,43 +22,44 @@ class RobotiqRTUClient:
                                          baudrate = 115200,
                                          timeout = 0.2)
         self.client.connect() # this is always true?
-    
+        # TODO: use request_status() to check if connection is alive
+
     def disconnect(self):
         if self.client != None:
             self.client.close()
-    
+
     def send_command(self, command):
         # make sure data has an even number of elements
         if(len(command) % 2 == 1):
             command.append(0)
-        
+
         # empty output register value to write
         output = []
-        
+
         # fill message by combining two bytes in one register
         for i in range(0, len(command)//2):
             byte1 = command[2 * i] << 8
             byte2 = command[2 * i + 1]
             output.append(byte1 + byte2)
-            
+
         # sending the command
         # TODO: catch ModbusIOException when writing powered off
         self.client.write_registers(self.input_register_addr, output, unit=self.unit_id)
 
-    
+
     def request_status(self, nbytes = 6, retries = 50):
         nregs = int(ceil(nbytes // 2.0))
         # TODO: catch ModbusIOException when reading powered off
         status = self.client.read_holding_registers(self.output_register_addr, nregs, unit = self.unit_id, timeout = 3)
         return self.parse_register(status, nregs)
-        
+
     def parse_register(self, recv_regs, nregs):
         output = []
         for i in range(nregs):
             output.append((recv_regs.getRegister(i) & 0xFF00) >> 8)
-            output.append( recv_regs.getRegister(i) & 0x00FF)           
+            output.append( recv_regs.getRegister(i) & 0x00FF)
         return output
-    
+
 class RobotiqTCPClient:
     def __init__(self):
         raise NotImplementedError
