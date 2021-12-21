b3ff402fda1f0a668a199b842276df33a8b4f17b
Minh Tram <minh.tram@mavs.uta.edu>
2021-12-21 00:45:29 -0600

    working autocomplete and analysis

--- catkin_ws/src/ur_remote_control/setup.py
+++ catkin_ws/src/ur_remote_control/setup.py
@@ -5,10 +5,12 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['custom_utilities',
+    packages=['ur_remote_control',
+              'custom_utilities',
               'ur_remote_dashboard',
               'robotiq_controller'],
-    package_dir={'custom_utilities': 'src/custom_utilities',
+    package_dir={'': 'src',
+                 'custom_utilities': 'src/custom_utilities',
                  'ur_remote_dashboard': 'src/ur_remote_dashboard',
                  'robotiq_controller': 'src/robotiq_controller'})
 
