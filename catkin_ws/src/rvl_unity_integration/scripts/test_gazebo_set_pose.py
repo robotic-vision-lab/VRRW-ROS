f36b20baac2fe1c2b0745c42d4cf3287060eef87
Minh Tram <minh.tram@mavs.uta.edu>
2022-04-21 18:44:06 -0500

    added placeholder files, memory leaking issue with ROS TCP Endpoint

--- catkin_ws/src/rvl_unity_integration/scripts/test_gazebo_set_pose.py
+++ catkin_ws/src/rvl_unity_integration/scripts/test_gazebo_set_pose.py
@@ -1,34 +1,44 @@
 #!/usr/bin/env python3
 
+from typing import final
 import rospy
 import rospkg
 from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import SetModelState
+from geometry_msgs.msg import Pose
 
-def main():
-    rospy.init_node('set_pose')
-
+def update_gazebo_pose(pose_msg:Pose):
     state_msg = ModelState()
     state_msg.model_name = 'hmd_camera'
-    state_msg.pose.position.x = 1.0
-    state_msg.pose.position.y = 1.0
-    state_msg.pose.position.z = 1.0
-    state_msg.pose.orientation.x = 0
-    state_msg.pose.orientation.y = 0
-    state_msg.pose.orientation.z = 0
-    state_msg.pose.orientation.w = 0
+    state_msg.pose.position.x = pose_msg.position.x
+    state_msg.pose.position.y = pose_msg.position.y
+    state_msg.pose.position.z = pose_msg.position.z
+    state_msg.pose.orientation.x = pose_msg.orientation.x
+    state_msg.pose.orientation.y = pose_msg.orientation.y
+    state_msg.pose.orientation.z = pose_msg.orientation.z
+    state_msg.pose.orientation.w = pose_msg.orientation.w
 
-    rospy.wait_for_service('/gazebo/set_model_state')
     try:
         set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
-        resp = set_state( state_msg )
-        print('Pose is set!')
-
+        _ = set_state( state_msg )
     except rospy.ServiceException as e:
         print(f"Service call failed: {e}")
+    finally:
+        del pose_msg
+        del state_msg
+
+def main():
+    rospy.init_node('hmd_pose_updater_node', anonymous=True)
+    rospy.wait_for_message('/unity/hmd_pose', Pose)
+    rospy.wait_for_service('/gazebo/set_model_state')
+
+    _ = rospy.Subscriber('/unity/hmd_pose', Pose, callback=update_gazebo_pose)
+
+    rospy.spin()
 
 if __name__ == '__main__':
     try:
         main()
     except rospy.ROSInterruptException:
         pass
+
