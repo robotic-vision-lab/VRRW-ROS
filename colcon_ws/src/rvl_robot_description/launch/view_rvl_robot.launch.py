263bf8c47e3d1a906e732a081657ac4d05dd8602
Minh Tram <minh.tram@mavs.uta.edu>
2022-09-02 17:35:43 -0500

    added ROS2 description of lab's robot

--- colcon_ws/src/rvl_robot_description/launch/view_rvl_robot.launch.py
+++ colcon_ws/src/rvl_robot_description/launch/view_rvl_robot.launch.py
@@ -1,23 +1,26 @@
-from launch_ros.actions import Node, SetParameter
-from launch_ros.substitutions import FindPackageShare
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, OpaqueFunction
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration, LocalSubstitution
+import os
 
-# TODO: Make this auto switch between 2f-85 and 2f-140
+from launch import LaunchDescription
+from launch.substitutions import Command, PathJoinSubstitution, FindExecutable
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.descriptions import ParameterValue
+
 def generate_launch_description():
     robot_description_content = Command(
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
-            PathJoinSubstitution([FindPackageShare("rvl_robot_description"), "xacro", "robotiq", "robotiq_2f_140.xacro"]),
+            PathJoinSubstitution([FindPackageShare("rvl_robot_description"), "xacro", "rvl_ur5e.xacro"]),
         ]
     )
+
+    robot_description_content = ParameterValue(robot_description_content, value_type=str)
 
     robot_description = {"robot_description": robot_description_content}
 
     rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("rvl_robot_description"), "rviz", "view_robotiq.rviz"]
+        [FindPackageShare("rvl_robot_description"), "rviz", "view_rvl_ur5e.rviz"]
     )
 
     joint_state_publisher_node = Node(
