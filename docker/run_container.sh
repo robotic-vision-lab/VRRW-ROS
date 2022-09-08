25d83356bea6e8f449c5e0caed7871e6990a9ddb
Minh Tram <minh.tram@mavs.uta.edu>
2022-09-08 00:22:04 -0500

    added fetch xacro, broken upon Unity import

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,54 +1,59 @@
 #!/usr/bin/env bash
 
-# courtesy of https://github.com/NVlabs/Deep_Object_Pose/blob/master/docker/run_dope_docker.sh
+# TODO: check if Docker is installed
+# TODO: check OS and Linux distribution to dynamically swap docker run and exec
 
-# get the script parent directory
+# get the full path of the script parent directory
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# location of catkin_ws on host machine in relation to script
-HOST_CATKIN_WS=$( cd "$( dirname -- "${BASH_SOURCE[0]}" )/../catkin_ws/src" >/dev/null 2>&1 ; pwd -P )
+# extension of usage of SCRIPT_DIR
+# get the path of the included colcon_ws
+HOST_CATKIN_WS=$( cd "${SCRIPT_DIR}/../catkin_ws/src" &> /dev/null && pwd )
 
 # name of the container to be created
-CONTAINER_NAME=unity-robosim-container
+CONTAINER_NAME=ros1-robosim-container
 
 # Docker image and tag to use
-IMAGE_NAME=unity-robosim
-IMAGE_TAG=cudagl_noetic
+IMAGE_NAME=robosim
+IMAGE_TAG=noetic-desktop-focal-cuda
 
 # get docker container ID if exists
 CONTAINER_ID=`docker ps -aqf "name=^/${CONTAINER_NAME}$"`
 
 # if container with name not found
 if [ -z "${CONTAINER_ID}" ]; then
+
     # creating the docker container
     docker run -t -d \
-    --name ${CONTAINER_NAME} \
     --privileged \
-    --runtime nvidia \
+    --name ${CONTAINER_NAME} \
     --gpus all \
     --shm-size 16G \
-    --network host \
-    -e "DISPLAY=$DISPLAY" \
-    -v "/tmp/.X11-unix:/tmp/.X11-unix" \
-    -v "${HOST_CATKIN_WS}:/root/catkin_ws/src" \
+    --device /dev/dxg \
+    -p 50000-50003:50000-50003 \
+    -p 54321:54321 \
+    -p 10000:10000 \
+    -e DISPLAY=$DISPLAY \
+    -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+    -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+    -e PULSE_SERVER=$PULSE_SERVER \
+    -v /usr/lib/wsl:/usr/lib/wsl \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /mnt/wslg:/mnt/wslg \
+    -v ${HOST_CATKIN_WS}:/root/catkin_ws/src \
     ${IMAGE_NAME}:${IMAGE_TAG} \
     bash
 
     # add convenient aliases
     docker cp ${SCRIPT_DIR}/.bash_aliases ${CONTAINER_NAME}:/root/.bash_aliases
+
 else
-    # allow X Server access
-    xhost +local:`docker inspect --format='{{ .Config.Hostname }}' ${CONTAINER_ID}`
 
     # Check if the container is already running and start if necessary.
     if [ -z `docker ps -qf "name=^/${CONTAINER_NAME}$"` ]; then
-        echo "${CONTAINER_NAME} container not running. Starting container..."
         docker start ${CONTAINER_ID}
-    else
-        echo "Attaching to running ${CONTAINER_NAME} container..."
     fi
+
     docker exec -it ${CONTAINER_ID} bash
 
-    # remove previous access
-    xhost -local:`docker inspect --format='{{ .Config.Hostname }}' ${CONTAINER_ID}`
 fi
